50b637473095207fd41139bb2931fa9ef3e5f0a4
hohserg <hohserg1@yandex.ru>
2019-09-28 22:52:18 +0300

    Faked proof of concept: VERY generic combiner
    Using shapeless for make combiner dependent on requirements parameter

--- src/main/scala/hohserg/baked/quad/lens/lambda/Combiners.java
+++ src/main/scala/hohserg/baked/quad/lens/lambda/Combiners.java
@@ -1,5 +1,11 @@
 package hohserg.baked.quad.lens.lambda;
 
+import hohserg.baked.quad.lens.immutable.VertexGetLens;
+import hohserg.baked.quad.lens.immutable.VertexSetLens;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+
+import java.awt.*;
+
 public class Combiners {
     public interface Float3Combiner<A> {
         A apply(float x, float y, float z);
@@ -19,5 +25,11 @@ public class Combiners {
 
     public interface Short2Combiner<A> {
         A apply(short x, short y);
+    }
+
+    public static void test(){
+        BakedQuad someQuad=null;
+        Color colorOfFourthVertex = VertexGetLens.getColor(someQuad.getVertexData(), someQuad.getFormat(), 3, Color::new);
+
     }
 }
