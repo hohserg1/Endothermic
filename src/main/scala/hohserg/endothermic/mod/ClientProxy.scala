ab62b80caae8714f8a95537a2bf8c3dcc7581945
hohserg <hohserg1@yandex.ru>
2020-04-18 19:53:05 +0300

    Added test for finding quads which uv are not co-directed with uv of atlas

--- src/main/scala/hohserg/endothermic/mod/ClientProxy.scala
+++ src/main/scala/hohserg/endothermic/mod/ClientProxy.scala
@@ -1,6 +1,6 @@
 package hohserg.endothermic.mod
 
-import hohserg.endothermic.immutable.UnpackedQuad
+import hohserg.endothermic.quad.immutable.UnpackedQuad
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
@@ -8,9 +8,10 @@ import net.minecraft.world.World
 import net.minecraftforge.client.event.ModelBakeEvent
 import net.minecraftforge.fml.client.registry.ClientRegistry
 import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.eventhandler.{EventPriority, SubscribeEvent}
 
 import scala.collection.JavaConverters._
+import scala.util.Try
 
 class ClientProxy extends CommonProxy {
 
@@ -30,18 +31,20 @@ class ClientProxy extends CommonProxy {
 
   override def getClientGuiElement(ID: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): AnyRef = new GuiSelectBechmark(world.getTileEntity(new BlockPos(x, y, z)).asInstanceOf[TestingStand.TileTestingStand])
 
-  @SubscribeEvent
+  @SubscribeEvent(priority = EventPriority.LOWEST)
   def onBakeModel(e: ModelBakeEvent): Unit = {
     val models = e.getModelRegistry.getKeys.asScala.map(e.getModelRegistry.getObject)
     val quads = models.flatMap(m =>
-      EnumFacing.values().flatMap(f =>
-        m.getQuads(null, f, 0).asScala.toList)
+      EnumFacing.values().toSeq.flatMap(f =>
+        Try(m.getQuads(null, f, 0).asScala.toList.map(identity)).toOption).flatten
     )
-    val noneTrivialQuads = quads.map(UnpackedQuad.apply).filter { q =>
-      (q.v1_u == q.v2_u && q.v3_u == q.v4_u &&
-        q.v1_v == q.v3_v && q.v2_v == q.v4_v) ||
-        (q.v1_u == q.v3_u && q.v2_u == q.v4_u &&
-          q.v1_v == q.v2_v && q.v3_v == q.v4_v)
+    val noneTrivialQuads = quads.map(UnpackedQuad.apply).filterNot { q =>
+
+      (q.v1_u == q.v2_u && q.v4_u == q.v3_u &&
+        q.v1_v == q.v4_v && q.v2_v == q.v3_v) ||
+        (q.v1_u == q.v4_u && q.v2_u == q.v3_u &&
+          q.v1_v == q.v2_v && q.v4_v == q.v3_v)
+
     }
 
     println("noneTrivialQuads", noneTrivialQuads)
