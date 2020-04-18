5b1cfd8734a968db6031802ae49f09f5a986ba18
hohserg <hohserg1@yandex.ru>
2020-04-18 19:54:12 +0300

    Added lenses for quad attributes

---
+++ src/main/scala/hohserg/endothermic/lense/QuadAttributeLens.scala
@@ -0,0 +1,18 @@
+package hohserg.endothermic.lense
+
+import hohserg.endothermic.format.AttributeRepresentation.AttributeId
+import hohserg.endothermic.format.UnpackEvaluations
+import net.minecraft.client.renderer.block.model.BakedQuad
+
+object QuadAttributeLens {
+
+  def get(attributeId: AttributeId, quad: BakedQuad): Float = {
+    UnpackEvaluations.getFormatParseRule(quad.getFormat).get(attributeId).map(_._1(quad)).getOrElse(0)
+  }
+
+  def set(attributeId: AttributeId, quad: BakedQuad, value: Float): Unit = {
+    UnpackEvaluations.getFormatParseRule(quad.getFormat).get(attributeId).foreach(_._2(value, quad.getVertexData))
+  }
+
+
+}
