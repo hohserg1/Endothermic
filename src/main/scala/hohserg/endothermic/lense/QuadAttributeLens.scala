ba40c3866bd7ac155b077f06e1ce1ff186601346
hohserg <hohserg1@yandex.ru>
2020-05-16 14:16:58 +0300

    Improve lenses

--- src/main/scala/hohserg/endothermic/lense/QuadAttributeLens.scala
+++ src/main/scala/hohserg/endothermic/lense/QuadAttributeLens.scala
@@ -1,26 +1,38 @@
 package hohserg.endothermic.lense
 
 import hohserg.endothermic.format.AttributeRepresentation.AttributeId
-import hohserg.endothermic.format.UnpackEvaluations
+import hohserg.endothermic.format.UnpackEvaluations.{AttributeIsomorphism, _}
 import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.vertex.VertexFormat
 
-object QuadAttributeLens {
+class QuadAttributeLens(rules: Map[AttributeId, AttributeIsomorphism]) {
 
-  def get(attributeId: AttributeId, quad: BakedQuad): Float =
-    UnpackEvaluations.getFormatParseRule(quad.getFormat).get(attributeId).map(_.unpack(quad.getVertexData)).getOrElse(0)
-
+  def get(attributeId: AttributeId, quad: BakedQuad): Float = {
+    val option = rules.get(attributeId)
+    if (option.isDefined)
+      option.get.unpack(quad.getVertexData)
+    else
+      0
+  }
 
   def set(attributeId: AttributeId, quad: BakedQuad, value: Float): Unit =
-    UnpackEvaluations.getFormatParseRule(quad.getFormat).get(attributeId).foreach(_.pack(value, quad.getVertexData))
+    rules.get(attributeId).foreach(_.pack(value, quad.getVertexData))
 
-  def get(attributeId: AttributeId, format:VertexFormat,vertexData:Array[Int]): Float =
-    UnpackEvaluations.getFormatParseRule(format).get(attributeId).map(_.unpack(vertexData)).getOrElse(0)
+  def get(attributeId: AttributeId, vertexData: Array[Int]): Float = {
+    val option = rules.get(attributeId)
+    if (option.isDefined)
+      option.get.unpack(vertexData)
+    else
+      0
+  }
 
+  def set(attributeId: AttributeId, vertexData: Array[Int], value: Float): Unit =
+    rules.get(attributeId).foreach(_.pack(value, vertexData))
 
-  def set(attributeId: AttributeId, format:VertexFormat,vertexData:Array[Int], value: Float): Unit =
-    UnpackEvaluations.getFormatParseRule(format).get(attributeId).foreach(_.pack(value, vertexData))
+}
 
+object QuadAttributeLens {
 
+  val getForFormat: VertexFormat => QuadAttributeLens = memoize(format => new QuadAttributeLens(getFormatParseRule(format)))
 
 }
